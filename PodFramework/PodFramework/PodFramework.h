f83c34e4a89d4f40a1a64733a39994c7cbb21cee
Arun Thirunavukarassu <arun.thirunavukarassu@cognizant.com>
2019-05-13 17:46:34 +0530

    finished

---
+++ PodFramework/PodFramework/PodFramework.h
@@ -0,0 +1,19 @@
+//
+//  PodFramework.h
+//  PodFramework
+//
+//  Created by Thirunavukarassu, Arun (Cognizant) on 13/05/19.
+//  Copyright © 2019 Cognizant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodFramework.
+FOUNDATION_EXPORT double PodFrameworkVersionNumber;
+
+//! Project version string for PodFramework.
+FOUNDATION_EXPORT const unsigned char PodFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodFramework/PublicHeader.h>
+
+
---
+++ PodFramework/PodFramework/Service.swift
@@ -0,0 +1,18 @@
+//
+//  Service.swift
+//  PodFramework
+//
+//  Created by Thirunavukarassu, Arun (Cognizant) on 13/05/19.
+//  Copyright © 2019 Cognizant. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init(){}
+    
+    public static func PrintLn() -> String {
+        return "-------------------------------"
+    }
+}
